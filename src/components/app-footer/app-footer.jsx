07efbccd385bb7e770c0031bd9be6fedfd48e6be
John <robinsonkukuruzo@gmail.com>
2024-04-17 11:32:32 -0700

    Fix bugs

--- src/components/app-footer/app-footer.jsx
+++ src/components/app-footer/app-footer.jsx
@@ -6,7 +6,7 @@ class AppFooter extends Component {
 
   render() {
     return (
-      <footer className="bg-white dark:bg-gray-900 mt-10">
+      <footer className="bg-white dark:bg-gray-900 mt-80">
         <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
           <div className="md:flex md:justify-between">
             <div className="mb-6 md:mb-0">
